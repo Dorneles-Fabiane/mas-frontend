8de261c2370d5b60a052650623682fcc6f353111
Fabiane Dorneles <fabiane.mail.d@gmail.com>
2021-05-20 12:10:54 -0300

    feat: add Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Container } from "../Button/styles";
+import { Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container> 
+
+      <Content>
+        <h1>My Activity Space</h1> 
+
+        <div>
+          <button type="button">
+            New curricular unit
+          </button>
+          <button type="button">
+            New Activity
+          </button>
+        </div>
+      </Content>
+
+    </Container>
+  );
+}
