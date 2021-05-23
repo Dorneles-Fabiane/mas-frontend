42876cf3fd656f4e37daa5461d3bb65bb846dadf
Fabiane Dorneles <fabiane.mail.d@gmail.com>
2021-05-23 14:41:39 -0300

    refactor: Routing structure - added the application pages  * 'Dashboard', 'Login', Register' and 'NotFound' *  at the routes index file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+
 import { Switch, Route } from 'react-router-dom';
-import login from '../pages/Login';
+
+import { Dashboard } from '../pages/Dashboard';
+import { Login } from '../pages/Login';
 import { NotFound } from '../pages/NotFound';
+import { Register } from '../pages/Register';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={login} />
-    <Route path="/*" component={NotFound}/>
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/" exact component={Login} />
+    <Route path="/register" component={Register} />
+    <Route path="/*" component={NotFound} />
   </Switch>
 );
 
